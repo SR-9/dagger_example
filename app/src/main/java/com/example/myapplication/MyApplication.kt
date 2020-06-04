65fd2ca6e64f9d88f196c5f3c1e6cc84045a20e1
HieuTV <hieutv@hybrid-technologies.co.jp>
2020-06-04 16:25:31 +0700

    - test Koin

--- app/src/main/java/com/example/myapplication/MyApplication.kt
+++ app/src/main/java/com/example/myapplication/MyApplication.kt
@@ -5,9 +5,14 @@ import android.content.Context
 import com.example.myapplication.feature.authentication.loginViewModelModule
 import com.example.myapplication.network.networkModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.Koin
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.logger.Level
 import org.koin.dsl.module
+import java.util.*
 
 class MyApplication : Application(){
 
@@ -16,8 +21,9 @@ class MyApplication : Application(){
         super.onCreate()
 
         startKoin {
-            androidLogger()
             androidContext(this@MyApplication)
+            androidLogger(level = Level.INFO)
+            androidFileProperties(koinPropertyFile = "koin.properties")
             modules(listOf(
                 prefModule,
                 prefModule2,
@@ -30,18 +36,19 @@ class MyApplication : Application(){
 }
 
 val prefModule = module {
-    single { ExamplePreferences(androidContext()) }
+    single { ExamplePreferences(androidContext(), getProperty("name")) }
 }
 
 val prefModule2 = module {
     single { ExamplePreferences2(get()) }
 }
 
-class ExamplePreferences(context: Context) {
+class ExamplePreferences(context: Context, val name: String) {
 
     var a = 0
     fun greeting() {
         a += 1
+        println(name)
         println("1ExamplePreferences $a")
     }
 }
